041950d0e3bb39378e3effe954ff6491c2977548
DongheePaul <dlehdgml0480@naver.com>
2023-07-24 22:19:22 +0900

    Implement api call to get customer information

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://picsum.photos/id/64/200",
-    name: "김유나",
-    birthday: "961222",
-    gender: "여자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://picsum.photos/id/65/200",
-    name: "김윤아",
-    birthday: "960305",
-    gender: "여자",
-    job: "프로그래머",
-  },
-  {
-    id: 3,
-    image: "https://picsum.photos/id/91/200",
-    name: "이순신",
-    birthday: "921205",
-    gender: "남자",
-    job: "디자이너",
-  },
-];
-
 class App extends Component {
+  //state는 변경될 수 있는 데이터, props는 변경될 수 없는 데이터 명시할 때.
+  state = {
+    customers: "",
+  };
+
+  //모든 컴포넌트가 mount되었을 때 실행됨.
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -64,19 +54,21 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                ></Customer>
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    ></Customer>
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
