7a54882e24c363367287076a8168e09067d8c04e
DongheePaul <dlehdgml0480@naver.com>
2023-07-28 00:53:27 +0900

    Seperate Customer from the App and make it component. And applied Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,257 +1,37 @@
 import React, { Component } from "react";
-import Customer from "./components/Customer";
-import CustomerAdd from "./components/CustomerAdd";
 import "./App.css";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import { withStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import { fade } from "@material-ui/core/styles/colorManipulator";
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
+import Customer from "./components/Customer";
+import Mypage from "./page/mypage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>{" "}
+              {/* Link 컴포넌트를 이용하여 경로를 연결한다 */}
+            </li>
+            <li>
+              <Link to="/customer">Customer</Link>
+            </li>
+            <li>
+              <Link to="/mypage">Mypage</Link>
+            </li>
+          </ul>
+        </nav>
 
-const styles = (theme) => ({
-  root: {
-    width: "100%",
-    minWidth: 1080,
-  },
-  progress: {
-    margin: theme.spacing(2),
-  },
-  menu: {
-    marginTop: 15,
-    marginBottom: 15,
-    display: "flex",
-    justifyContent: "center",
-  },
-  paper: {
-    marginLeft: 18,
-    marginRight: 18,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  tableHead: {
-    fontSize: "1.0rem",
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-    marginLeft: 30,
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing.unit,
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-    width: "100%",
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 200,
-      },
-    },
-  },
-});
-
-const pages = ["Products", "Pricing", "Blog"];
-
-class App extends Component {
-  //state = 컴포넌트 내부에서 관리되는 변경 가능한 데이터. props는 부모-> 자식 컴포넌트로 전달되는 읽기 전용 데이터.
-  constructor(props) {
-    super(props);
-    this.state = {
-      customers: "",
-      completed: 0,
-      searchKeyword: "",
-    };
-  }
-
-  stateRefresh = () => {
-    this.setState({
-      customers: "",
-      completed: 0,
-      searchKeyword: "",
-    });
-    this.callApi()
-      .then((res) => this.setState({ customers: res }))
-      .catch((err) => console.log(err));
-  };
-
-  //모든 컴포넌트가 mount되었을 때 실행됨.
-  componentDidMount() {
-    this.timer = setInterval(this.progress, 20);
-    this.callApi()
-      .then((res) => this.setState({ customers: res }))
-      .catch((err) => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch("/api/customers");
-    const body = await response.json();
-    return body;
-  };
-
-  progress = () => {
-    const { completed } = this.state;
-    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
-  };
-
-  handleValueChange = (e) => {
-    let nextState = {};
-    nextState[e.target.name] = e.target.value;
-    this.setState(nextState);
-  };
-
-  render() {
-    const filteredComponent = (data) => {
-      data = data.filter((customer) => {
-        return customer.name.indexOf(this.state.searchKeyword) > -1;
-      });
-      return data.map((c) => {
-        return (
-          <Customer
-            stateRefresh={this.stateRefresh}
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        );
-      });
-    };
-    const { classes } = this.props;
-    const cellList = [
-      "번호",
-      "프로필",
-      "이름",
-      "생년월일",
-      "성별",
-      "직업",
-      "설정",
-    ];
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar disableGutters>
-            <Typography
-              className={classes.title}
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              color="inherit"
-            >
-              Home
-            </Typography>
-            <Typography
-              className={classes.title}
-              variant="h6"
-              color="inherit"
-              noWrap
-              component="a"
-              href="/"
-            >
-              고객 관리 시스템
-            </Typography>
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="검색하기"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                name="searchKeyword"
-                value={this.state.searchKeyword}
-                onChange={this.handleValueChange}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-        <div className={classes.menu}>
-          <CustomerAdd stateRefresh={this.stateRefresh} />
-        </div>
-        <Paper className={classes.paper}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                {cellList.map((c) => {
-                  return (
-                    <TableCell className={classes.tableHead}>{c}</TableCell>
-                  );
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers ? (
-                filteredComponent(this.state.customers)
-              ) : (
-                <TableRow>
-                  <TableCell colSpan="6" align="center">
-                    <CircularProgress
-                      className={classes.progress}
-                      variant="determinate"
-                      value={this.state.completed}
-                    />
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </Paper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/customer" element={<Customer />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
-
-export default withStyles(styles)(App);
+function Home() {
+  return <h1>Home</h1>;
+}
+export default App;
